95c6e59816319b174f1c35b24c797e2ff2de8441
jazz <jscott85@student.cnm.edu>
2026-06-02 14:21:54 -0600

    Implemented Scanner-based conversion

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -1,18 +1,48 @@
 package edu.cnm.deepdive;
 
+import java.util.Scanner;
+
 public class Main {
 
   void main(String[] args) {
-    // TODO: 6/2/26 Set initial conversion mode to Celsius-to-Fahrenheit.
-    // TODO: 6/2/26 Start reading from standard input.
+    // DONE: 6/2/26 Set initial conversion mode to Celsius-to-Fahrenheit.
+    Mode mode = Mode.CELSIUS_TO_FAHRENHEIT;
+    Converter converter = new Converter();
+    // DONE: 6/2/26 Start reading from standard input.
+    Scanner scanner = new Scanner(System.in);
     // TODO: 6/2/26 While there is any input remaining to process;
-    // TODO: 6/2/26 1. Get next input token.
+    while (scanner.hasNext()) {
+      if (scanner.hasNextDouble()) {
+        double input = scanner.nextDouble();
+        double output;
+        if (mode == Mode.CELSIUS_TO_FAHRENHEIT) {
+          output = converter.convertC2F(input);
+        } else {
+          output = converter.convertF2C(input);
+        }
+        // TODO: 6/2/26 Print conversion mode, inout, output.
+        System.out.println("input = " + input + "; output =" + output);
+      } else {
+        String input = scanner.next();
+        char modeFlag = Character.toUpperCase(input.strip().charAt(0));
+        if (modeFlag == 'C') {
+          mode = Mode.CELSIUS_TO_FAHRENHEIT;
+        } else if (modeFlag == 'F') {
+          mode = Mode.FAHRENHEIT_TO_CELSIUS;
+        } else {
+          // TODO: 6/2/26 Print error message to standard error device.
+        }
+      }
+    }
+    // DONE: 6/2/26 1. Get next input token.
     //                 a. if token is a number, convert according to the current mode, and output mode to standard output
     //                 b. if token is 'F', switch mode to Fahrenheit-to-Celsius for subsequent conversions.
     //                 c. if token is 'C', switch mode to Celsius-to-Fahrenheit for subsequent conversions.
     //                 d. if token is anything else, write error message to standard error.
+  }
 
-
-   }
+  private enum Mode {
+    CELSIUS_TO_FAHRENHEIT, FAHRENHEIT_TO_CELSIUS
+  }
 
 }
