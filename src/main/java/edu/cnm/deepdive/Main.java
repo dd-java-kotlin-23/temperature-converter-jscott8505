de1dcd5e74f0d0cd23be6f77547608489ef0f16c
jazz <jscott85@student.cnm.edu>
2026-06-02 11:02:30 -0600

    Added TODO comments for dynamic conversion mode and input processing

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -3,13 +3,16 @@ package edu.cnm.deepdive;
 public class Main {
 
   void main(String[] args) {
-//    double[] celsiusInputs = {0, 100, -40};
-   Converter converter = new Converter();
-    for (String arg : args) {
-      double c = Double.parseDouble(arg);
-      double f = converter.convertC2F(c);
-      System.out.printf("Celsius = %.2f; Fahrenheit = %.2f%n", c, f);
-    }
-  }
+    // TODO: 6/2/26 Set initial conversion mode to Celsius-to-Fahrenheit.
+    // TODO: 6/2/26 Start reading from standard input.
+    // TODO: 6/2/26 While there is any input remaining to process;
+    // TODO: 6/2/26 1. Get next input token.
+    //                 a. if token is a number, convert according to the current mode, and output mode to standard output
+    //                 b. if token is 'F', switch mode to Fahrenheit-to-Celsius for subsequent conversions.
+    //                 c. if token is 'C', switch mode to Celsius-to-Fahrenheit for subsequent conversions.
+    //                 d. if token is anything else, write error message to standard error.
+
+
+   }
 
 }
