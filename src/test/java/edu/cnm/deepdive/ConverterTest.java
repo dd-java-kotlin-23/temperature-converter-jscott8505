00d32e34411f3783d9644187c5e2e80a0c79b35d
jazz <jscott85@student.cnm.edu>
2026-06-02 10:48:22 -0600

    Added test class w/ simple celsius-to-fahrenheit test.

---
+++ src/test/java/edu/cnm/deepdive/ConverterTest.java
@@ -0,0 +1,28 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class ConverterTest {
+
+  private static final double TOLERANCE = 0.000001;
+
+  @Test
+  void convertC2F() {
+    Converter converter = new Converter();
+    double celsiusFreezingPoint = 0;
+    double fahrenheitFreezingPoint = 32;
+    double actual = converter.convertC2F(celsiusFreezingPoint);
+    assertEquals(fahrenheitFreezingPoint, actual, TOLERANCE);
+    //TODO Figure out how to test multiple inputs & actual outputs vs. expected outputs, without writing a lot of code.
+  }
+
+  @Disabled
+  @Test
+  void convertF2C() {
+    fail("Test not implemented"); // FIXME implement this test.
+  }
+
+}
